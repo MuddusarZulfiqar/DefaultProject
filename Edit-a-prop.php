362586dc4be911dddd790ac025d201812ce0ddb9
atifkuwaitnet <info@logicsdrive.com>
2021-04-22 12:34:18 +0500

    Footer Update

---
+++ Edit-a-prop.php
@@ -0,0 +1,151 @@
+<?php include_once "includes/header.php";?>
+<div class="EditPopup section">
+    <div class="pageHead">
+        <div class="container">
+            <h1>Edit Your <b>Property</b></h1>
+        </div>
+    </div>
+    <div class="EditPopup__main">
+        <div class="custom_container">
+            <div class="property">
+                <h2>Property Details</h2>
+                <div class="row mt-30">
+                    <div class="col-6">
+                        <div class="form-group">
+                            <div>
+                                <input type="text" class="form-control" placeholder="Property Name (English)">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                    <div class="form-group">
+                            <div>
+                                <input type="text" class="form-control" placeholder="Property Name (Arabic) ">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label class="control-label">Property Type</label>
+                            <div>
+                                <select class="selectpicker">
+                                    <option>Office</option>
+                                    <option>2 Week</option>
+                                    <option>3 Week</option>
+                                    <option>4 Week</option>
+                                    <option>5 Week</option>
+                                    <option>6 Week</option>
+                                    <option>7 Week</option>
+                                    <option>8 Week</option>
+                                    <option>9 Week</option>
+                                    <option>10 Week</option>
+                                    <option>1 Year</option>
+                                    <option>2 Year</option>
+                                    <option>3 Year</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <ul class="list-inline">
+                                <li class="radio list-inline-item">
+                                    <label>
+                                        <input type="radio" name="radio" checked="">
+                                        <span>Exchange</span>
+                                    </label>
+                                </li>
+                                <li class="radio list-inline-item">
+                                    <label>
+                                        <input type="radio" name="radio">
+                                        <span>Buy</span>
+                                    </label>
+                                </li>
+                                <li class="radio list-inline-item">
+                                    <label>
+                                        <input type="radio" name="radio">
+                                        <span>Rent</span>
+                                    </label>
+                                </li>
+                                <li class="radio list-inline-item">
+                                    <label>
+                                        <input type="radio" name="radio">
+                                        <span>Sell</span>
+                                    </label>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label class="control-label">Property Type</label>
+                            <div>
+                                <select class="selectpicker">
+                                    <option>Office</option>
+                                    <option>2 Week</option>
+                                    <option>3 Week</option>
+                                    <option>4 Week</option>
+                                    <option>5 Week</option>
+                                    <option>6 Week</option>
+                                    <option>7 Week</option>
+                                    <option>8 Week</option>
+                                    <option>9 Week</option>
+                                    <option>10 Week</option>
+                                    <option>1 Year</option>
+                                    <option>2 Year</option>
+                                    <option>3 Year</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label class="control-label">Property Type</label>
+                            <div>
+                                <select class="selectpicker">
+                                    <option>Office</option>
+                                    <option>2 Week</option>
+                                    <option>3 Week</option>
+                                    <option>4 Week</option>
+                                    <option>5 Week</option>
+                                    <option>6 Week</option>
+                                    <option>7 Week</option>
+                                    <option>8 Week</option>
+                                    <option>9 Week</option>
+                                    <option>10 Week</option>
+                                    <option>1 Year</option>
+                                    <option>2 Year</option>
+                                    <option>3 Year</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="radio" checked="">
+                                    <span class="icon">
+                                        <svg id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.8 512">
+                                            <use xlink:href="#mosque"></use>
+                                        </svg>
+                                    </span>
+                                    <span class="label">Mosque</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include_once "includes/footer.php";?>
--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,38 @@
     <footer>
         <div class="container">
-            <div class="logo">
-                <a href="" class="text-center">
-                    <div class="d-flex justify-content-center">
-                        <img src="./src/assets/logo.png" alt="">
+            <div class="footer">
+                <div class="footer__logo">
+                    <a href="" class="footer__logo--img">
+                        <img src="./src/images/footer_logo.png" alt="">
+                    </a>
+                </div>
+                <div class="footer__list">
+                    <ul>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Questions</a></li>
+                        <li><a href="">Contact</a></li>
+                        <li><a href="">Privacy Policy</a></li>
+                        <li><a href="">Terms of use</a></li>
+                    </ul>
+                
+                </div>
+                <div class="footer__foot">
+                    <div class="footer__foot--copyright">
+                        <p>©2021Aqarat.All Rights Reserved.</p>
                     </div>
-                </a>
+                    <div class="footer__foot--social">
+                        <ul>
+                            <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                            <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                            <li><a href=""><i class="fab fa-google-plus-g"></i></a></li>
+                            <li><a href=""><i class="fab fa-youtube"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="footer__foot--img">
+                        <img src="./src/images/footer__icon.png" alt="">
+                    </div>
+                </div>
             </div>
         </div>
     </footer>
