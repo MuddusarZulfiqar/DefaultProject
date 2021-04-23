ca371ff68bb5e752bfa7ed313dbdcecb430e5cc7
atifkuwaitnet <info@logicsdrive.com>
2021-04-23 16:44:01 +0500

    new updates

---
+++ 10-01-my-dashboard-my-properties.php
@@ -0,0 +1,238 @@
+<?php include_once "includes/header.php";?>
+<div class="content-wrapper">
+    <div class="pageHead">
+        <div class="container">
+            <div class="d-flex align-items-center justify-content-between">
+                <h1>My<span>Properties</span></h1>
+                <a href="#" class="btn btn-secondary">Add a propertry</a>
+            </div>
+        </div>
+    </div>
+    <div class="properties">
+        <div class="container">
+            <form>
+                <div class="filterRow">
+                    <div class="relative">
+                        <label class="control-label">Filter By</label>
+                        <select class="selectpicker">
+                            <option>Archived</option>
+                            <option>In-progress</option>
+                            <option>Completed</option>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-primary">go!</button>
+                </div>
+            </form>
+            <div class="table">
+                <div class="table__head">
+                    <div class="table__row">
+                        <div class="table__row--cell" data-width="10">
+                        Property ID
+                        </div>
+                        <div class="table__row--cell text-left" data-width="20">
+                        Property Name
+                        </div>
+                        <div class="table__row--cell">
+                        Category
+                        </div>
+                        <div class="table__row--cell">
+                        For?
+                        </div>
+                        <div class="table__row--cell">
+                        Price (KD)
+                        </div>
+                        <div class="table__row--cell">
+                        Deal Price (KD)
+                        </div>
+                        <div class="table__row--cell  text-left">
+                        Status
+                        </div>
+                        <div class="table__row--cell">
+                        Actions
+                        </div>
+                    </div>
+                </div>
+                <div class="table__body">
+                    <div class="table__row">
+                        <div class="table__row--cell" data-width="10" data-label="Property ID">1000</div>
+                        <div class="table__row--cell text-left" data-label=" Property Name" data-width="20">
+                        Sunset Villa - Beverly Hills 
+                        </div>
+                        <div class="table__row--cell" data-label="Category">
+                        House
+                        </div>
+                        <div class="table__row--cell" data-label="For?">
+                        Exchange
+                        </div>
+                        <div class="table__row--cell" data-label="Price (KD)">
+                        3000
+                        </div>
+                        <div class="table__row--cell" data-label="Deal Price (KD)">
+                        0
+                        </div>
+                        <div class="table__row--cell text-left" data-label="Status">
+                            <div class="status green">
+                                <svg fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 320.001 320.001" style="enable-background:new 0 0 320.001 320.001;" xml:space="preserve">
+                                <use xlink:href="#play-icon"></use>
+                                </svg>
+                                Open
+                            <p>Expires in 39 Days</p>
+                            </div>
+                        </div>
+                        <div class="table__row--cell" data-label="Actions">
+                            <ul class="action-btns">
+                                <li class="edit">
+                                    <a href="#">
+                                    <svg fill="currentColor" height="492pt" viewBox="0 0 492.49284 492" width="492pt" xmlns="http://www.w3.org/2000/svg">
+                                        <use xlink:href="#edit-icon"></use>
+                                    </svg>      
+                                    </a>
+                                </li>
+                                <li class="delete">
+                                    <a href="#">
+                                        <svg fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 384 384" style="enable-background:new 0 0 384 384;" xml:space="preserve">
+                                            <use xlink:href="#delete-icon"></use>
+                                        </svg>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="table__row">
+                        <div class="table__row--cell" data-width="10" data-label="Property ID">1021</div>
+                        <div class="table__row--cell text-left" data-label=" Property Name" data-width="20">
+                        Rose Apartment 
+                        </div>
+                        <div class="table__row--cell" data-label="Category">
+                        Apartments
+                        </div>
+                        <div class="table__row--cell" data-label="For?">
+                        Buy
+                        </div>
+                        <div class="table__row--cell" data-label="Price (KD)">
+                        56.000
+                        </div>
+                        <div class="table__row--cell" data-label="Deal Price (KD)">
+                        34.500
+                        </div>
+                        <div class="table__row--cell text-left" data-label="Status">
+                            <div class="status blue">
+                            <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="52.8014mm" height="59.3963mm" version="1.1" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd" viewBox="0 0 1227 1380.25" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xodm="http://www.corel.com/coreldraw/odm/2003">
+                                <use xlink:href="#progress-icon"></use>
+                                </svg>
+                                In-progress
+                            </div>
+                        </div>
+                        <div class="table__row--cell" data-label="Actions">
+                            <ul class="action-btns">
+                                <li class="edit">
+                                    <a href="#">
+                                    <svg fill="currentColor" height="492pt" viewBox="0 0 492.49284 492" width="492pt" xmlns="http://www.w3.org/2000/svg">
+                                        <use xlink:href="#edit-icon"></use>
+                                    </svg>      
+                                    </a>
+                                </li>
+                                <li class="delete">
+                                    <a href="#">
+                                        <svg fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 384 384" style="enable-background:new 0 0 384 384;" xml:space="preserve">
+                                            <use xlink:href="#delete-icon"></use>
+                                        </svg>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="table__row">
+                        <div class="table__row--cell" data-width="10" data-label="Property ID">1099</div>
+                        <div class="table__row--cell text-left" data-label=" Property Name" data-width="20">
+                        Al Awadhi Offices 12 floors 
+                        </div>
+                        <div class="table__row--cell" data-label="Category">
+                        Office
+                        </div>
+                        <div class="table__row--cell" data-label="For?">
+                        Rent
+                        </div>
+                        <div class="table__row--cell" data-label="Price (KD)">
+                        12000
+                        </div>
+                        <div class="table__row--cell" data-label="Deal Price (KD)">
+                        12000
+                        </div>
+                        <div class="table__row--cell text-left" data-label="Status">
+                            <div class="status red">
+                            <svg fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="973.199px" height="973.199px" viewBox="0 0 973.199 973.199" style="enable-background:new 0 0 973.199 973.199;" xml:space="preserve">
+                                <use xlink:href="#archiev-icon"></use>
+                                </svg>
+                                Archived
+                            </div>
+                        </div>
+                        <div class="table__row--cell" data-label="Actions">
+                            <ul class="action-btns">
+                                <li class="edit">
+                                    <a href="#">
+                                    <svg fill="currentColor" height="492pt" viewBox="0 0 492.49284 492" width="492pt" xmlns="http://www.w3.org/2000/svg">
+                                        <use xlink:href="#edit-icon"></use>
+                                    </svg>      
+                                    </a>
+                                </li>
+                                <li class="delete">
+                                    <a href="#">
+                                        <svg fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 384 384" style="enable-background:new 0 0 384 384;" xml:space="preserve">
+                                            <use xlink:href="#delete-icon"></use>
+                                        </svg>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="table__row">
+                        <div class="table__row--cell" data-width="10" data-label="Property ID">1899</div>
+                        <div class="table__row--cell text-left" data-label=" Property Name" data-width="20">
+                        Al Reem Penthouse 
+                        </div>
+                        <div class="table__row--cell" data-label="Category">
+                        House
+                        </div>
+                        <div class="table__row--cell" data-label="For?">
+                        Buy
+                        </div>
+                        <div class="table__row--cell" data-label="Price (KD)">
+                        5000
+                        </div>
+                        <div class="table__row--cell" data-label="Deal Price (KD)">
+                        0
+                        </div>
+                        <div class="table__row--cell text-left" data-label="Status">
+                            <div class="status darkblue">
+                            <svg fill="currentColor" height="417pt" viewBox="0 -46 417.81333 417" width="417pt" xmlns="http://www.w3.org/2000/svg">
+                                <use xlink:href="#check-icon"></use>
+                                </svg>
+                                Completed
+                            </div>
+                        </div>
+                        <div class="table__row--cell" data-label="Actions">
+                            <ul class="action-btns">
+                                <li class="edit">
+                                    <a href="#">
+                                    <svg fill="currentColor" height="492pt" viewBox="0 0 492.49284 492" width="492pt" xmlns="http://www.w3.org/2000/svg">
+                                        <use xlink:href="#edit-icon"></use>
+                                    </svg>      
+                                    </a>
+                                </li>
+                                <li class="delete">
+                                    <a href="#">
+                                        <svg fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 384 384" style="enable-background:new 0 0 384 384;" xml:space="preserve">
+                                            <use xlink:href="#delete-icon"></use>
+                                        </svg>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    </div>
+</div>
+<?php include_once "includes/footer.php";?> 
--- @index.php
+++ @index.php
@@ -1,7 +1,7 @@
 <?php $ishome = 1; include_once "includes/header.php";?>      
     <div class="content-wrapper">
     	<div class="searchProperty">
-            <div class="semiCircle"></div>
+            <div class="semiCircle"></div> 
             <div class="bg"></div>
             <div class="container">
                 <h1 class="text-center">Find Your <span>Perfect place</span></h1>
@@ -10,6 +10,53 @@
                 </div>
             </div>
             
+        </div>
+        <div class="invest">
+            <div class="container">
+                <h2 class="sectionTitle">How To Invest</h2>
+                <div class="slick-instance" data-slick='{"slidesToShow": 4, "slidesToScroll": 1}'>
+                    <div class="invest__item">
+                        <div class="invest__item--image">
+                            <img src="src/images/invest/invest-img1.png" alt="img1">
+                        </div>
+                        <div class="invest__item--title">Sell a Property</div>
+                        <div class="invest__item--desc">
+                            <p>Lorem Ipsum is simply dummy is the text of the printing.</p>
+                        </div>
+                        <a href="#" class="btn btn-primary">start now</a>
+                    </div>
+                    <div class="invest__item">
+                        <div class="invest__item--image">
+                            <img src="src/images/invest/invest-img2.png" alt="img1">
+                        </div>
+                        <div class="invest__item--title">Rent a Property</div>
+                        <div class="invest__item--desc">
+                            <p>Lorem Ipsum is simply dummy is the text of the printing.</p>
+                        </div>
+                        <a href="#" class="btn btn-primary">start now</a>
+                    </div>
+                    <div class="invest__item">
+                        <div class="invest__item--image">
+                            <img src="src/images/invest/invest-img3.png" alt="img1">
+                        </div>
+                        <div class="invest__item--title">Buy a Property</div>
+                        <div class="invest__item--desc">
+                            <p>Lorem Ipsum is simply dummy is the text of the printing.</p>
+                        </div>
+                        <a href="#" class="btn btn-primary">start now</a>
+                    </div>
+                    <div class="invest__item">
+                        <div class="invest__item--image">
+                            <img src="src/images/invest/invest-img4.png" alt="img1">
+                        </div>
+                        <div class="invest__item--title">Exchange a Properties</div>
+                        <div class="invest__item--desc">
+                            <p>Lorem Ipsum is simply dummy is the text of the printing.</p>
+                        </div>
+                        <a href="#" class="btn btn-primary">start now</a>
+                    </div>
+                </div>
+            </div>
         </div>
    	</div>
 <?php include_once "includes/footer.php";?>
