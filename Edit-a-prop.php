8d6caa040ec30e78d8600125d4aa5dc189f5b0b5
atifkuwaitnet <info@logicsdrive.com>
2021-04-23 16:48:21 +0500

    Muddusar Update

--- Edit-a-prop.php
+++ Edit-a-prop.php
@@ -6,22 +6,18 @@
         </div>
     </div>
     <div class="EditPopup__main">
-        <div class="custom_container">
+        <div class="container-md">
             <div class="property">
                 <h2>Property Details</h2>
                 <div class="row mt-30">
                     <div class="col-6">
                         <div class="form-group">
-                            <div>
-                                <input type="text" class="form-control" placeholder="Property Name (English)">
-                            </div>
+                            <input type="text" class="form-control" placeholder="Property Name (English)">
                         </div>
                     </div>
                     <div class="col-6">
                     <div class="form-group">
-                            <div>
-                                <input type="text" class="form-control" placeholder="Property Name (Arabic) ">
-                            </div>
+                            <input type="text" class="form-control" placeholder="Property Name (Arabic) ">  
                         </div>
                     </div>
                 </div>
@@ -29,7 +25,6 @@
                     <div class="col-6">
                         <div class="form-group">
                             <label class="control-label">Property Type</label>
-                            <div>
                                 <select class="selectpicker">
                                     <option>Office</option>
                                     <option>2 Week</option>
@@ -45,13 +40,10 @@
                                     <option>2 Year</option>
                                     <option>3 Year</option>
                                 </select>
-                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-12">
-                        <div class="form-group">
+                <div class="form-group">
                             <ul class="list-inline">
                                 <li class="radio list-inline-item">
                                     <label>
@@ -79,8 +71,6 @@
                                 </li>
                             </ul>
                         </div>
-                    </div>
-                </div>
                 <div class="row">
                     <div class="col-6">
                         <div class="form-group">
@@ -107,40 +97,226 @@
                     <div class="col-6">
                         <div class="form-group">
                             <label class="control-label">Property Type</label>
-                            <div>
-                                <select class="selectpicker">
-                                    <option>Office</option>
-                                    <option>2 Week</option>
-                                    <option>3 Week</option>
-                                    <option>4 Week</option>
-                                    <option>5 Week</option>
-                                    <option>6 Week</option>
-                                    <option>7 Week</option>
-                                    <option>8 Week</option>
-                                    <option>9 Week</option>
-                                    <option>10 Week</option>
-                                    <option>1 Year</option>
-                                    <option>2 Year</option>
-                                    <option>3 Year</option>
-                                </select>
-                            </div>
+                            <select class="selectpicker">
+                                <option>Office</option>
+                                <option>2 Week</option>
+                                <option>3 Week</option>
+                                <option>4 Week</option>
+                                <option>5 Week</option>
+                                <option>6 Week</option>
+                                <option>7 Week</option>
+                                <option>8 Week</option>
+                                <option>9 Week</option>
+                                <option>10 Week</option>
+                                <option>1 Year</option>
+                                <option>2 Year</option>
+                                <option>3 Year</option>
+                            </select>
                         </div>
                     </div>
                 </div>
-                <div class="row">
+                <div class="row radio_row">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="" class="control-label">Property Features</label>
+                        </div>
+                    </div>
                     <div class="col-md-4">
                         <div class="form-group">
                             <div class="checkbox">
                                 <label>
                                     <input type="checkbox" name="radio" checked="">
                                     <span class="icon">
-                                        <svg id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.8 512">
+                                        <svg fill="currentColor" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.8 512">
                                             <use xlink:href="#mosque"></use>
                                         </svg>
                                     </span>
                                     <span class="label">Mosque</span>
                                 </label>
                             </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="radio" checked="">
+                                    <span class="icon">
+                                        <svg fill="currentColor" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.8 512">
+                                            <use xlink:href="#mosque"></use>
+                                        </svg>
+                                    </span>
+                                    <span class="label">Mosque</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="radio" checked="">
+                                    <span class="icon">
+                                        <svg fill="currentColor" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.8 512">
+                                            <use xlink:href="#mosque"></use>
+                                        </svg>
+                                    </span>
+                                    <span class="label">Mosque</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="radio" checked="">
+                                    <span class="icon">
+                                        <svg fill="currentColor" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.8 512">
+                                            <use xlink:href="#mosque"></use>
+                                        </svg>
+                                    </span>
+                                    <span class="label">Mosque</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="radio" checked="">
+                                    <span class="icon">
+                                        <svg fill="currentColor" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.8 512">
+                                            <use xlink:href="#mosque"></use>
+                                        </svg>
+                                    </span>
+                                    <span class="label">Mosque</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="radio" checked="">
+                                    <span class="icon">
+                                        <svg fill="currentColor" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.8 512">
+                                            <use xlink:href="#mosque"></use>
+                                        </svg>
+                                    </span>
+                                    <span class="label">Mosque</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="radio" checked="">
+                                    <span class="icon">
+                                        <svg fill="currentColor" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.8 512">
+                                            <use xlink:href="#mosque"></use>
+                                        </svg>
+                                    </span>
+                                    <span class="label">Mosque</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="radio" checked="">
+                                    <span class="icon">
+                                        <svg fill="currentColor" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.8 512">
+                                            <use xlink:href="#mosque"></use>
+                                        </svg>
+                                    </span>
+                                    <span class="label">Mosque</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="radio" checked="">
+                                    <span class="icon">
+                                        <svg fill="currentColor" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.8 512">
+                                            <use xlink:href="#mosque"></use>
+                                        </svg>
+                                    </span>
+                                    <span class="label">Mosque</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="radio" checked="">
+                                    <span class="icon">
+                                        <svg fill="currentColor" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.8 512">
+                                            <use xlink:href="#mosque"></use>
+                                        </svg>
+                                    </span>
+                                    <span class="label">Mosque</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="radio" checked="">
+                                    <span class="icon">
+                                        <svg fill="currentColor" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.8 512">
+                                            <use xlink:href="#mosque"></use>
+                                        </svg>
+                                    </span>
+                                    <span class="label">Mosque</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="radio" checked="">
+                                    <span class="icon">
+                                        <svg fill="currentColor" id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.8 512">
+                                            <use xlink:href="#mosque"></use>
+                                        </svg>
+                                    </span>
+                                    <span class="label">Mosque</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Beds ">  
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Baths ">  
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Kitchen ">  
                         </div>
                     </div>
                 </div>
