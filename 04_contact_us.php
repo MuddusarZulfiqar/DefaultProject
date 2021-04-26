76f8f0b1f8a153a2b186ea2e3c64d91da9d999fe
atifkuwaitnet <info@logicsdrive.com>
2021-04-26 16:38:26 +0500

    ayyan updates

--- 04_contact_us.php
+++ 04_contact_us.php
@@ -88,14 +88,46 @@
                 </form>
                 
             </div>
-            <div class="col-6">
+            <div class="col-6 card_position">
+                <div class="card">
+                    <div class="card--icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="32" height="43" viewBox="0 0 32 43">
+                            <use xlink:href="#pin"></use>
+                        </svg>
+                    </div>
+                    <div class="card--title">
+                        <p>15th Floor, Jassim Al Mahmoud Tower,<br/>Part A<br/>Ahmad Al Jaber Street Block 11<br/>Shaheed Gardens, Kuwait City, Kuwait 
+                        </p>
+                    </div>
+                </div>
                 <div class="map">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23400.264532978978!2d47.94663136832136!3d29.327214702730082!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3fcf9be5e5831bd7%3A0xaa52f7ad503a636c!2sMahamoud%20AL-Gharabally%20trading%20Est.%20Showroom!5e0!3m2!1sen!2s!4v1619424404277!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                 </div>
                 <div class="contact">
-                    <div class="contact__item"></div>
-                    <div class="contact__item"></div>
-                    <div class="contact__item"></div>
+                    <div class="contact__item">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="38" height="38" viewBox="0 0 38 38">
+                                <use xlink:href="#cell"></use>
+                            </svg>
+                        </span>
+                        <span>00965  1234 5678</span>
+                    </div>
+                    <div class="contact__item">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="40" height="40" viewBox="0 0 40 40">
+                                <use xlink:href="#letter"></use>
+                            </svg>
+                        </span>
+                        <span>Info@aqarat.com</span>
+                    </div>
+                    <div class="contact__item">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="44" height="44" viewBox="0 0 44 44">
+                                <use xlink:href="#telephone"></use>
+                            </svg>
+                        </span>
+                        <span>00965  1234 5600</span>
+                    </div>
                 </div>
             </div>
         </div>
