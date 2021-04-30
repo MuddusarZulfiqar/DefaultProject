647417960b88e00b81b00f969b75f09d9c139a16
atifkuwaitnet <info@logicsdrive.com>
2021-04-30 13:18:26 +0500

    sell page responsive update by ayyan

--- 13-01-sell-page.php
+++ 13-01-sell-page.php
@@ -7,7 +7,7 @@
     </div>
     <div class="container-md">
         <div class="row  align-items-center" >
-            <div class="col-md-6">
+            <div class="col-sm-6">
                 <h2>
                     Sell your <br/> <span>property with Us</span>
                 </h2>
@@ -15,19 +15,21 @@
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. 
                 </p>
             </div>
-            <div class="col-md-6">
-                <img src="src/images/sell_page.png" alt="" class="img-fluid">
+            <div class="col-sm-6">
+                <div class="image">
+                    <img src="src/images/sell_page.png" alt="" class="img-fluid">
+                </div>
             </div>
         </div>
         <div class="card-wrapper row">
-            <div class="col-md-6">
+            <div class="col-sm-6">
                 <div class="card">
                     <h3 class="card-title">Register with Us</h3>
                     <p class="card-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste asperiores consectetur nam! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste asperiores consectetur nam! </p>
                     <a href="" class="btn btn-primary card-button">Start Now</a>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-sm-6">
                 <div class="card">
                     <h3 class="card-title">Register with Us</h3>
                     <p class="card-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste asperiores consectetur nam! </p>
