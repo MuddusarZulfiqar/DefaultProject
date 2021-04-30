0b74b4c892b9dc9c9573274f826389f6487334ab
Kashif Arif <kashif@kuwaitnet.com>
2021-04-30 16:29:34 +0500

    style.scss updates

--- 09-02-my-saves.php
+++ 09-02-my-saves.php
@@ -7,8 +7,8 @@
     </div>
     <div class="container">
         <div class="properties">
-            <div class="row">
-                <div class="col-md-4">
+            <div class="row justify-content-center">
+                <div class="col-lg-4 col-sm-6">
                     <div class="property__item">
                         <div class="property__item--image">
                             <img src="src/images/house.png" alt="" class="img-fluid">
@@ -69,7 +69,68 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-lg-4 col-sm-6">
+                    <div class="property__item">
+                        <div class="property__item--image">
+                            <img src="src/images/house.png" alt="" class="img-fluid">
+                            <div class="overlay">
+                                <a href="#" class="btn btn-secondary">explore</a>
+                            </div>
+                            <span class="property__item--type">buy</span>
+                            <span class="property__item--save"></span>
+                            <span class="property__item--delete">
+                            <svg width="30px" height="30px" fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                            viewBox="0 0 384 384" style="enable-background:new 0 0 384 384;" xml:space="preserve">
+                                <use xlink:href="#delete-icon"></use>
+                            </svg>
+                        </span>
+                        </div>
+                        <div class="property__info">
+                            <a href="" class="property__info--title mb-3">sui house</a>
+                            <p class="property__info--location mb-3"><img class="icon" src="src/images/svg/location.svg" alt="" > Ahmed Al Jaber Street , Sahrq</p>
+                            <div class="property__info--features mb-3">
+                                <ul>
+                                    <li>
+                                        <div class="icon">
+                                            <img src="src/images/svg/bed.svg" alt="">
+                                        </div>
+                                        <div>
+                                            6
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="icon">
+                                            <img src="src/images/svg/bath.svg" alt="">
+                                        </div>
+                                        <div class="icons_label">
+                                            3
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="icon">
+                                            <img src="src/images/svg/kitchen.svg" alt="">
+                                        </div>
+                                        <div>
+                                            3
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="property__organizor">
+                            <div class="d-flex align-items-center">
+                                <div class="property__organizor--image">
+                                    <img  src="src/images/card_logo.png" alt="" class="img-fluid rounded-circle">
+                                </div>
+                                <div class="property__organizor--name">
+                                    <span>BY</span>
+                                    Gulf Investment Co.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
                     <div class="property__item">
                         <div class="property__item--image">
                             <img src="src/images/house.png" alt="" class="img-fluid">
